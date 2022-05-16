6e023556937ec106e1ea0999a82c19ccc5513b38
Jacob Doren <Jacob Doren>
2022-05-15 21:02:16 -0400

    added gravity

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const ctx = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight 
 
+const gravity = 0.5
+
 class Player {
-    constructor() {
+    constructor(gravity) {
         this.position = {
             x: 100,
             y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 0
         }
         this.width = 100
         this.height = 100
@@ -18,8 +24,24 @@ class Player {
         ctx.fillStyle = 'red'
         ctx.fillRect(this.position.x, this.position.y,this.width, this.height)
     }
+
+    update() {
+        this.position.y += this.velocity.y
+        console.log(canvas.height)
+        if(this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0
+        } else this.velocity.y += gravity
+        this.draw()
+    }
 }
 
 const player = new Player()
 
-player.draw()
+// Loop and update player
+function animate() {
+    requestAnimationFrame(animate)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    player.update()
+}
+
+animate()
